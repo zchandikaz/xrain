105fbce4d5a14a25aa18b637bf3b5c2139906518
MadhuraDilanka <agmdsampath@gmail.com>
2020-01-05 10:09:18 +0530

    now successfully receiving an image/file

--- lib/scan.dart
+++ lib/scan.dart
@@ -15,7 +15,12 @@ class ScanPage extends StatefulWidget {
 }
 
 class _ScanPageState extends State<ScanPage> {
-  String result = "Hi";
+
+  String syncTempId;
+  int bulkSize, bulkCount;
+  String receivedStr = "";
+  DatabaseReference sendStatusRef;
+  String result = "wait";
 
   Future scanQR() async{
     try{
@@ -34,59 +39,63 @@ class _ScanPageState extends State<ScanPage> {
     }
   }
 
-  void pairDevice(@deprecated void onConnected(String syncTempId, int bulkSize)) async {
-    String syncTempId = await scanQR();
+  void pairDevice(@deprecated void onConnected()) async {
+    syncTempId = await scanQR();
     var initStatusRef = dbRef.child('sync_tmp').child(syncTempId).child('init_status');
+    sendStatusRef = dbRef.child('sync_tmp').child(syncTempId).child('send_status');
 
-    try {
-      dbRef.child('sync_tmp').child(syncTempId).once().then((
-          DataSnapshot snapshot) {
-        if (snapshot.value == null) return;
+    var sRef = dbRef.child('sync_tmp').child(syncTempId);
+    sRef.once().then((
+      DataSnapshot snapshot) {
+      if (snapshot.value == null) return;
 
-        var status = snapshot.value["init_status"];
-        CA.log("scan pairing - $status");
-        if (status == 0) {
-          initStatusRef.set(1);
+      var status = snapshot.value["init_status"];
+      CA.log("scan pairing - $status");
+      if (status == 0) {
+        initStatusRef.set(1);
 
-          onConnected(syncTempId, snapshot.value["bulk_size"]);
-        }
-      });
-    }catch (e){
-      CA.log("ERROR: ${e.toString()}");
-    }
+        bulkSize = snapshot.value["bulk_size"];
+        onConnected();
+      }
+    });
   }
 
   String unwrap(String str){
-    CA.log(str);
     RegExp exp = new RegExp(r"☺(.*)☺");
     List<Match> matches = exp.allMatches(str).toList();
     if(matches.length==1){
       var s = matches[0].group(0);
+      CA.log(s.substring(1,s.length-1));
       return s.substring(1,s.length-1);
     }else{
       throw Exception("Format mismatch of data bulk");
     }
   }
 
-  receiveBulk(String syncTempId, String str, int bulkCount, int bulkSize) async {
-    var sendStatusRef = dbRef.child('sync_tmp').child(syncTempId).child('send_status');
-
+  onReceiveStatusChanged(v){
     sendStatusRef.once().then((DataSnapshot snapshot) async {
-      var status = snapshot.value??0;
+      var status = snapshot.value;
+      if(status==null) return;
+      status = double.parse(status.toString());
+      if(status-status.toInt()>0) return;
+      status = status.toInt();
       CA.log("receive bulk| $bulkCount, $status");
       if(status==bulkCount+1) {
         String qrData = await scanQR();
         sendStatusRef.set(status + 0.5);
+        CA.log("status changed by 0.5");
         try {
-          receiveBulk(
-              syncTempId, str + unwrap(qrData), bulkCount + 1, bulkSize);
+          bulkCount++;
+          receivedStr += unwrap(qrData);
+          CA.log(receivedStr);
         }catch(e){
           CA.log("ERROR: ${e.toString()}");
           sendStatusRef.set(-2);
           return;
         }
       }else if(status==-1){
-        storeReceivedFile(str);
+        CA.log("stored");
+        storeReceivedFile();
       }else if(status==-2){
         CA.alert(context, "Sharing Aborted!");
         return;
@@ -95,19 +104,29 @@ class _ScanPageState extends State<ScanPage> {
   }
 
   Uint8List decodeData(String data){
-     Uint8List.fromList(CA.splitByLength(data, 2).map((String h){
+    List<int> intList = CA.splitByLength(data, 2).map((String h){
       if(h[0]=='0')
         h = h[1];
       return int.parse(h, radix: 16);
-    }));
+    }).toList();
+    //return new Uint8List(0);
+    return Uint8List.fromList(intList);
   }
 
-  void storeReceivedFile(String str) async {
-    var d = str.split("☻▬☻");
+  void storeReceivedFile() async {
+    var d = receivedStr.split("☻▬☻");
     String fileName =  d[0];
     Uint8List fileData = decodeData(d[1]);
     Directory dir = await getExternalStorageDirectory();
-    writeToFile(fileData, dir.path + "/xrain/" + fileName);
+
+//    SecurityContext clientContext = new SecurityContext()
+//        ..setTrustedCertificates(dir.path + "/xrain");
+//    var client = new HttpClient(context: clientContext);
+
+    CA.log(dir.path +"/"+ fileName);
+    writeToFile(fileData, dir.path + "/" + fileName).then((v){
+      CA.navigateWithoutBack(context, Pages.home);
+    });
   }
 
   Future<void> writeToFile(Uint8List data, String path) {
@@ -116,9 +135,11 @@ class _ScanPageState extends State<ScanPage> {
         buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
   }
 
-  void receiveFile() async {
-    pairDevice((String syncTempId, int bulkSize){
-      receiveBulk(syncTempId, "", -1, bulkSize);
+  void beginReceiveFile() async {
+    pairDevice((){
+      receivedStr = "";
+      bulkCount = -1;
+      sendStatusRef.onValue.listen(onReceiveStatusChanged);
     });
   }
 
@@ -138,7 +159,7 @@ class _ScanPageState extends State<ScanPage> {
       floatingActionButton: FloatingActionButton.extended(
         icon: Icon(Icons.camera_alt),
         label: Text("Scan"),
-        onPressed: receiveFile,
+        onPressed: beginReceiveFile,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
